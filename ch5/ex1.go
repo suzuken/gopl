f9ab512576033d661e503077c0904498823dd50c
Kenta Suzuki <suzuken326@gmail.com>
2016-01-21 23:03:35 +0900

    5-1

---
+++ ch5/ex1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}
+
+func visit(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				fmt.Printf("%s\n", a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(c)
+	}
+}
